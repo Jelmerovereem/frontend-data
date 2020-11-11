23bc697628d018a96007eece1cf8527ef3000f0b
Jelmerovereem <jelmer_overeem@hotmail.com>
2020-11-11 16:13:13 +0100

    added script file for visualisation. Trying to get the right coordinates for cities

--- curranDatavizCourse/worldMap/scripts/script.js
+++ curranDatavizCourse/worldMap/scripts/script.js
@@ -4,7 +4,7 @@ fetch("https://geodata.nationaalgeoregister.nl/bag/wfs/v1_1?request=GetFeature&s
 .then(response => response.json())
 .then((data) => {
 	var stadData = data;
-	document.querySelector(".loading").innerText = "Done, object in console";
+	document.querySelector(".loading").innerText = "klaar, object in console";
 	renderMap(stadData);
 });
 
@@ -17,6 +17,12 @@ const projection = d3.geoMercator()
 	.translate([width/2, height/2]);
 const pathGenerator = d3.geoPath().projection(projection);
 
+//const gemeentes = topojson.feature(gemeenteData, gemeenteData.objects.gemeente_2020);
+//const provincies = topojson.feature(provinciedata, provinciedata.objects.provincie_2020);
+//const steden = topojson.feature(stadData, stadData.objects.stadsgewest_2015);
+//const wijken = topojson.feature(wijkData, wijkData.objects.wijk_2020);
+
+//console.log(wijken.features[0].geometry.coordinates[0][0])
 
 function renderMap(data) {
 	console.log(data);
@@ -32,3 +38,10 @@ function renderMap(data) {
 
 
 		//obj => obj.properties.statnaam
+
+/*svg.selectAll("path")
+	.data(gemeentes.features)
+	.enter().append("path")
+	.attr("d", pathGenerator)
+	.append("text")
+		.text(obj => obj.properties.statnaam)*/
