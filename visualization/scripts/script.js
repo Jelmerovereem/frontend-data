8f9c9f5e3d99dcf9773f8d9ad75bd25d1de441fa
Jelmerovereem <jelmer_overeem@hotmail.com>
2021-01-27 11:03:33 +0100

    fixed the update pattern & moved tooltip to seperate function

--- visualization/scripts/script.js
+++ visualization/scripts/script.js
@@ -185,66 +185,65 @@ function combineData(garageData, garageLocatieData, variableData) {
 const tooltip = document.querySelector(".tooltip");
 
 function renderPoints(coordinates) {
-	if (coordinates[0].hasOwnProperty("paid")) {
-		var circle = group.selectAll("circle")
-		.data(coordinates);
+	let circle = group.selectAll("circle");
 
-		circle.exit().remove();
-		circle.enter().append("circle")
-			.attr("r", 4)
-			.attr("transform", (obj) => {
-				return `translate(${projection([obj.long, obj.lat])})`;
-			});
-
-		circle.transition()
-			.duration(500)
-			.attr("fill", (obj) => {
-				if (/VERGUNNING|VERGUNP|VERGUN-ALG|VERGUN-MV/.test(obj.paid)) {
-					return "red"
-				} else if (/BETAALDP|GARAGEP/.test(obj.paid)) {
-					return "orange"
-				} else if (/onbekend/.test(obj.paid)) {
-					return "grey"
-				} else {
-					return "green"
-				}
-			})
-	} else {
-	var circle = group.selectAll("circle")
-	.data(coordinates);
-
-	if (!circle.empty()) {
-		circle.exit().remove();
-	}
-
-	circle.enter().append("circle")
-		.attr("r", 4)
-		.attr("transform", (obj) => {
-			return `translate(${projection([obj.long, obj.lat])})`;
-		})
-		.attr("fill", (obj) => {
-			if (obj.capacity <= 800) {
-				return "red"
-			} else if (obj.capacity > 800 && obj.capacity <= 1600) {
-				return "orange"
-			} else {
-				return "green"
-			}
-	});
+	circle
+		.data(coordinates, d => d.areaId)
+		.join(
+			enter => enter.append("circle")
+				.attr("r", 4)
+				.attr("transform", (obj) => {
+					return `translate(${projection([obj.long, obj.lat])})`
+				})
+				.attr("fill", (obj) => {
+					if (dropdown.value === "paid/free") {
+						if (/VERGUNNING|VERGUNP|VERGUN-ALG|VERGUN-MV/.test(obj.paid)) {
+							return "red"
+						} else if (/BETAALDP|GARAGEP/.test(obj.paid)) {
+							return "orange"
+						} else if (/onbekend/.test(obj.paid)) {
+							return "grey"
+						} else {
+							return "green"
+						}
+					} else if (dropdown.value === "capacity") {
+						if (obj.capacity <= 600) {
+							return "red"
+						} else if (obj.capacity > 600 && obj.capacity <= 1000) {
+							return "orange"
+						} else if (obj.capacity > 1000) {
+							return "green"
+						} else {
+							return "grey"
+						}
+					}
+				})
+				.attr("opacity", (d) => {
+					if (dropdown.value === "paid/free") {
+						if (/onbekend/.test(d.paid)) {
+							return .1
+						} else {
+							return 1
+						}
+					} else if (dropdown.value === "capacity") {
+						if (d.capacity === "onbekend") {
+							return .1
+						} else {
+							return 1
+						}
+					}
+				}),
+				exit => exit
+					.attr("fill", "white")
+					.call(exit => exit.transition().duration(500)
+						.attr("opacity", 0)
+						.remove())			
+			)
+		addTooltip()
 	
-	circle.transition()
-		.duration(500)
-		.attr("fill", (obj) => {
-				if (obj.capacity <= 800) {
-					return "red"
-				} else if (obj.capacity > 800 && obj.capacity <= 1600) {
-					return "orange"
-				} else {
-					return "green"
-				}
-		})
-	}
+}
 
+function addTooltip() {
 	group.selectAll("circle").on("mouseover", (event, obj) => {
 		tooltip.innerHTML = obj.areaDesc + "<br>";
 		if (obj.paid === undefined) {
@@ -252,15 +251,10 @@ function renderPoints(coordinates) {
 		} else {
 			tooltip.innerHTML += "Paid/free: " + obj.paid;	
 		}
-		
-		
-
 		tooltip.style.left = (event.pageX) + "px";
 		tooltip.style.top = (event.pageY + 10) + "px";
 		tooltip.classList.add("focus");
-		tooltip.style.opacity = "1";
-
-		
+		tooltip.style.opacity = "1";		
 	})
 	.on("mouseout", () => {
 		tooltip.style.opacity = "0";
@@ -317,7 +311,7 @@ function updateData() {
 	const variable = this.value;
 	if (variable === "capacity") {
 		document.querySelector(".first").innerText = "0";
-		document.querySelector(".last").innerText = "10";
+		document.querySelector(".last").innerText = "1700";
 		combineData(garageData, coordinatesArray, capacityData);
 	} else if (variable === "paid/free") {
 		document.querySelector(".first").innerText = "Betalen";
